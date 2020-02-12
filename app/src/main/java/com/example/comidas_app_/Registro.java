2bfda59820dc92a4037ea58484ca5edd19903194
cristiandsa <cristiandsa@gmail.com>
2020-02-11 22:51:54 -0500

    11-02-2020 22:51 mapa y registro

--- app/src/main/java/com/example/comidas_app_/Registro.java
+++ app/src/main/java/com/example/comidas_app_/Registro.java
@@ -12,22 +12,16 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Iterator;
 
 public class Registro extends AppCompatActivity {
     EditText nom, ape, usu, cel, con;
-    Button guar;
+    Button guar, miubi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,80 +34,92 @@ public class Registro extends AppCompatActivity {
         cel = findViewById(R.id.txtCel);
         con = findViewById(R.id.txtPwd);
         guar = findViewById(R.id.btnGuardar);
+        miubi = findViewById(R.id.btnMyGPS);
+
+        miubi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                miUbicacion();
+            }
+        });
 
         guar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                registrarUsuario();
+            public void onClick(View view) {
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        post();
+                    }
+                }).start();
+
             }
         });
+
     }
 
-    public void registrarUsuario() {
-        /*/Verificamos que las cajas de texto no esten vacías
-        if (null == usu.getText()) {
-            Toast.makeText(this, "Se debe ingresar un usuario", Toast.LENGTH_LONG).show();
-            return;
+    public void miUbicacion(){
+        Intent ubi = new Intent(Registro.this, MapsActivity.class );
+        startActivity(ubi);
+    }
+
+    public void  post ()
+    {
+        String sql ="https://appcomida.azurewebsites.net/api/Usuarios";
+        URL url = null;
+        try {
+            // se crea la conexion al api: http://localhost:15009/WEBAPIREST/api/persona
+            url = new URL(sql);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            //crear el objeto json para enviar por POST
+            JSONObject parametrosPost= new JSONObject();
+            parametrosPost.put("codigo", 0);
+            parametrosPost.put("nombre", nom.getText());
+            parametrosPost.put("apellido", ape.getText());
+            parametrosPost.put("celular", cel.getText());
+            parametrosPost.put("usuar", usu.getText());
+            parametrosPost.put("pass", con.getText());
+
+
+            //DEFINIR PARAMETROS DE CONEXION
+            urlConnection.setReadTimeout(15000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("POST");// se puede cambiar por delete ,put ,etc
+            urlConnection.setRequestProperty("Content-Type","application/json");
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+
+
+            DataOutputStream os = new DataOutputStream(urlConnection.getOutputStream());
+            //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+            os.writeBytes(parametrosPost.toString());
+
+            os.flush();
+            os.close();
+
+            int responseCode=urlConnection.getResponseCode();// conexion OK?
+            if(responseCode== 201)
+            {
+                Toast.makeText(this, "Hola soy un Toast", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Hola soy un Toast", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        if (null == nom.getText()) {
-            Toast.makeText(this, "Se debe ingresar un nombre", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (null == ape.getText()) {
-            Toast.makeText(this, "Se debe ingresar un apellido", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (null == cel.getText()) {
-            Toast.makeText(this, "Se debe ingresar un celular", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if (null == con.getText()) {
-            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_LONG).show();
-            return;
-        }
-*/
-        Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try  {
-                    String sql = "https://appcomida.azurewebsites.net/api/Usuarios";
-
-                    URL url = new URL(sql);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept", "application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-
-                    JSONObject parametrosPost = new JSONObject();
-                    parametrosPost.put("codigo", 0);
-                    parametrosPost.put("nombre", nom.getText());
-                    parametrosPost.put("apellido", ape.getText());
-                    parametrosPost.put("celular", cel.getText());
-                    parametrosPost.put("usuar", usu.getText());
-                    parametrosPost.put("pass", con.getText());
-
-
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-                    os.writeBytes(parametrosPost.toString());
-
-                    os.flush();
-                    os.close();
-
-                    conn.disconnect();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
-                }
-            }});
-
-        thread.start();
 
 
     }
-}
+
+
+    }
 
